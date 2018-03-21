2053a8698ac25582e96772e297b0c05dc73bd378
miths <mghinaiya@gmail.com>
2018-03-21 23:27:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 // change the te=xt of the main-text div
 var element= document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+//move the image
+
+var img= document.getElementById('img');
+img.style.marginleft='100px';
