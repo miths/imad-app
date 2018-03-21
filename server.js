73f5f0f5777fe8dc7d8c16b1d0803de60ae20016
miths <mghinaiya@gmail.com>
2018-03-21 19:27:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,50 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title: 'Article One | Mithil Ghinaiya'
+    heading: 'Article One'
+    date: 'Sep 5, 2018'
+    content: 'This is content for my first article.'
+}
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data,content;
+}
+var hrmlTemplate =`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+    </head>
+    
+    <body>
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3> 
+        ${heading}
+        </h3>
+        
+        <div>
+           ${date}
+        </div>
+        <div>
+            ${content}
+            
+        </div>
+    </body>
+</html>
+
+
+`;
+return htmlTemplate
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +58,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.send(createTemplate(articleOne))
 });
 
 app.get('/article-one', function (req, res) {
