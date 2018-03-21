f827a199a4ce790a774d1025acc16c2568e04544
miths <mghinaiya@gmail.com>
2018-03-21 23:54:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var img= document.getElementById('madi');
 var marginleft = 0;
 function moveRight(){
     marginLeft = marginLeft+5;
-    img.style.marginLeft = marginLeft+'px'
+    img.style.marginLeft = marginLeft+'px';
 }
 
 img.onclick = function(){
