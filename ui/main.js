c9973d64acb36c649a82b586ccb886f151734335
miths <mghinaiya@gmail.com>
2018-03-21 23:54:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ element.innerHTML = 'New value';
 //move the image
 
 var img= document.getElementById('madi');
-var marginleft = 0;
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft+5;
     img.style.marginLeft = marginLeft+'px';
